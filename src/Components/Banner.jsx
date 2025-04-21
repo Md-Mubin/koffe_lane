f9be73eef00f16fb39739722f4fdbfc53d67aa04
Md Mubin <fontdevmubin@gmail.com>
2025-04-21 23:11:08 +0600

    Changes to be committed: 	deleted:    public/file.svg 	deleted:    public/globe.svg 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	deleted:    public/window.svg 	new file:   src/Components/Banner.jsx 	deleted:    src/app/favicon.ico 	modified:   src/app/globals.css 	modified:   src/app/layout.js 	modified:   src/app/page.js

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <>
+            <section className='w-full h-[100dvh] bg-black'>
+                <div className="container">
+                    <ul className='bg-white'>
+sdf
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Banner
